3968d365cba95a35696c72b306891d45677e7c2f
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2025-02-20 17:09:05 -0300

    Adiciona nova view e template para exibir detalhes do produto; atualiza URLs e contexto da view index

--- core/views.py
+++ core/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render # type: ignore
 
+from core.models import Produto
+
+
 def index(request):
-    
-    print(dir(request))
-    print(F"User: {request.user}")
-    
-    if str(request.user) == 'AnonymousUser':
-        teste = "User is not authenticated"
-    else:
-        teste = "User is authenticated"
+    produtos = Produto.objects.all()
     context = {
         'curso': "Web progamming with Django Framework",
         'other': "Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of Web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.",
-        'logado' : teste
+        'produtos' : produtos
     }
     return render(request, 'index.html' , context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    
+    context = {
+        'produto': prod
+    } 
+    return render(request, 'produto.html', context)
