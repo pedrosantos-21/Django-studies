3a1a1861b17cf3817da739b79d83d68ec670b2e4
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2025-02-20 11:33:33 -0300

    atualizando views.py

--- core/views.py
+++ core/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
 def index(request):
+<<<<<<< HEAD
     print(dir(request))
     print(F"{request.method} - {request.path}")
     print(F"{request.scheme} - {request.get_host()}")
     print(F"{request.user} - {request.user.is_authenticated}")
     print(F"User-Agent: {request.headers} ")
+=======
+
+    print(dir(request))
+    print(F"User: {request.user}")
+    
+    if str(request.user) == 'AnonymousUser':
+        teste = "User is not authenticated"
+    else:
+        teste = "User is authenticated"
+>>>>>>> e7ada6c (atualizando views.py)
 
     context = {
         'curso': "Web progamming with Django Framework",
-        'other': "Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of Web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source."
+        'other': "Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of Web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.",
+        'logado' : teste
     }
     return render(request, 'index.html' , context)
 
