6b2174610611274900be37ae520fb7a15e083563
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2025-02-20 12:31:22 -0300

    Salvando mudanças locais antes do pull

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,7 @@ class produtoAdmin(admin.ModelAdmin): # Cria uma classe para o modelo Produto, q
     list_display = ('nome', 'preco', 'estoque') # Exibe os campos nome, preco e estoque
 
 class clienteAdmin(admin.ModelAdmin): # Cria uma classe para o modelo Client, que herda de admin.ModelAdmin
-    list_display = ('nome', 'sobre_nome', 'email') # Exibe os campos nome, sobrenome e email
+    list_display = ('nome', 'sobre_nome', 'email', 'data_nascimento') # Exibe os campos nome, sobrenome e email
 
 admin.site.register(Produto, produtoAdmin) # Registra o modelo Produto
 admin.site.register(Client, clienteAdmin) # Registra o modelo Client
--- django1/urls.py
+++ django1/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('painel/', admin.site.urls),
     path('', include('core.urls')), #Recomenadado criar um URLS.py para cada app
 ]
