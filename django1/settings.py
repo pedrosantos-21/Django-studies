4647077578f658f3310c3b481c074af70ca84859
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2025-06-27 09:01:03 -0300

    Configura ALLOWED_HOSTS no settings.py e desativa DEBUG

--- django1/settings.py
+++ django1/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-wcbunqy&$x55!2xzd^d068r+slk*13f32yn9bq0jt2&yp3px4(
 
 DEBUG = False #Quando for falso isso não vai exibir mesngaens de erro quando está em erro 
 
-ALLOWED_HOSTS = ["'django1-pv-57abc76057db.herokuapp.com', '.herokuapp.com'"]#isso precisa ser alterado para o ip do servidor
+ALLOWED_HOSTS = ['django1-pv-57abc76057db.herokuapp.com', '.herokuapp.com']#isso precisa ser alterado para o ip do servidor
  
 
 # Application definition
